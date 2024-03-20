d7cc2b460d67b7553ffc9acf061cf28594b9f5af
ioana-manghiuc <ioana-manghiuc@users.noreply.github.com>
2024-03-21 00:06:44 +0200

    Edit & Delete buttons created.

--- task-tracker/src/task-card/task-card.component.ts
+++ task-tracker/src/task-card/task-card.component.ts
@@ -4,17 +4,26 @@ import { Status } from '../status.enum';
 import {MatCardModule} from '@angular/material/card';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 import { NgFor } from '@angular/common';
 import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-task-card',
   standalone: true,
-  imports: [MatCardModule, MatSelectModule, MatFormFieldModule, NgFor],
+  imports: [MatCardModule, MatSelectModule, MatFormFieldModule, NgFor, MatIconModule],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.scss'
 })
 export class TaskCardComponent {
   @Input() task: Task | undefined;
   statusOptions: Status[] = Object.values(Status);
+  deleteTask(task: Task): void {
+    // logic
+  }
+
+  editTask(task: Task): void {
+    console.log('Editing task:', task);
+    // logic
+  }
 }
--- task-tracker/src/task-list/task-list.component.ts
+++ task-tracker/src/task-list/task-list.component.ts
@@ -2,12 +2,13 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Task } from '../task';
 import { NgFor, NgIf } from '@angular/common';
 import { FilterComponent } from '../filter/filter.component';
+import {MatIconModule} from '@angular/material/icon';
 import { Status } from '../status.enum';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [NgFor, NgIf, FilterComponent],
+  imports: [NgFor, NgIf, FilterComponent, MatIconModule],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
@@ -24,4 +25,17 @@ export class TaskListComponent implements OnInit {
   handleStatusSelected(status: Status) {
     this.filteredTasks = this.tasks.filter(task => task.status === status);
   }
+
+  deleteTask(task: Task): void {
+    const index = this.tasks.indexOf(task);
+    if (index !== -1) {
+      this.tasks.splice(index, 1);
+    }
+  }
+
+  editTask(task: Task): void {
+    console.log('Editing task:', task);
+    // logic
+  }
+
 }
