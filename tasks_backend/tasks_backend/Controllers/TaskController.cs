c6fbfd8ee4583dbe24479b7245705047ba2d341f
ioana-manghiuc <ioana-manghiuc@users.noreply.github.com>
2024-04-11 12:38:48 +0300

    Added ASP.NET Core application sln for backend.

---
+++ tasks_backend/tasks_backend/Controllers/TaskController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tasks_backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaskController : ControllerBase
+    {
+        [HttpGet(Name = "GetTasks")]
+        public ObjectResult Get()
+        {
+            return Ok("This is a list of tasks");
+        }
+
+        [HttpDelete(Name = "DeleteTask")]
+        public ObjectResult Delete()
+        {
+            return Ok("Task deleted");
+        }
+
+        [HttpPost(Name = "CreateTask")]
+        public ObjectResult Post()
+        {
+            return Ok("Task created");
+        }
+
+        [HttpPut(Name = "UpdateTask")]
+        public ObjectResult Put()
+        {
+            return Ok("Task updated");
+        }
+    }
+}
